b37606b64ceda43976ab50a595ba7001fd8247e2
Patrick Dolan <dolanp1992@gmail.com>
2022-03-02 20:50:37 -0800

    comment out tests
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- AnimalShelter.Tests/ModelTests/AnimalTests.cs
+++ AnimalShelter.Tests/ModelTests/AnimalTests.cs
@@ -1,14 +1,33 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AnimalShelter.Models;
+// using AnimalShelter.Models;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using MySql.Data.MySqlClient;
+// using System;
+// using System.Collections.Generic;
 
-namespace AnimalShelter.Tests
-{
-  [TestClass]
-  public class AnimalTests
-  {
-    // Test methods go here
-  }
-}
+// namespace AnimalShelter.Tests
+// {
+//   [TestClass]
+//   public class AnimalTests : IDisposable
+//   {
+//     public void Dispose()
+//     {
+//       Animal.CLearAll();
+//     }
+
+//     public AnimalTests()
+//     {
+//       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=animal_shelter_test;";
+//     }
+
+//     [TestMethod]
+//     public void GetAll_ReturnEmptyListFromDatabase_AnimalList()
+//     {
+//       List<Animal> newList = new List<Animal> {};
+//       List<Animal> result = Animal.GetAll();
+//       CollectionAssert.AreEqual(newList, result);
+//     }
+//   }
+// }
 // Example Test Method Structure
 //_________________________
 // [TestMethod]
