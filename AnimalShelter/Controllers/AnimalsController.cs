720370eb7e586b7ee73bb3faaba9697eeee98b64
Patrick Dolan <dolanp1992@gmail.com>
2022-03-06 13:00:41 -0800

    remove viewbag code

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -17,7 +17,6 @@ namespace AnimalShelter.Controllers
     public ActionResult Index()
     {
       List<Animal> model = _db.Animals.Where(animals => animals.Type == "Cat").ToList();
-      ViewBag.FavoriteColor = "green";
       return View(model);
     }
   }
